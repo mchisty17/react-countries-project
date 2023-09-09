0f788bff7432b0e9875f815710a37f0c4d5e2452
chistym17 <mchisty31@gmail.com>
2023-09-09 21:50:41 +0600

    data upload

--- react-countries/src/components/countries/countries.jsx
+++ react-countries/src/components/countries/countries.jsx
@@ -1,21 +1,26 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
-const countries = () => {
-const[country,setCountry]=useState([]);
+import Country from "../country/country";
+const Countries = () => {
+const[countries,setCountry]=useState([]);
 
 useEffect(()=>{
 
 fetch('https://restcountries.com/v3.1/all')
 .then(res=>res.json())
-.then(data=>console.log(data));
+.then(data=>setCountry(data));
 
 },[])
     return (
         <div>
+            <h3>All Country Data</h3>
+             {
+               countries.map(country=><Country key={country.cca3} country={country}></Country>)
+
+             }
             
         </div>
     );
 };
 
-export default countries;
+export default Countries;
