6e3e6f910f5c199632386ae8cd0f91c4741b6b1f
chistym17 <mchisty31@gmail.com>
2023-09-09 20:34:58 +0600

    first commit

---
+++ react-countries/src/App.jsx
@@ -0,0 +1,14 @@
+import Countries from "./components/countries/countries"
+
+function App() {
+
+  return (
+    <>
+     
+      <h1>Country Tour</h1>
+       <Countries> </Countries>
+    </>
+  )
+}
+
+export default App
---
+++ react-countries/src/components/countries/countries.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const countries = () => {
+const[country,setCountry]=useState([]);
+
+useEffect(()=>{
+
+fetch('https://restcountries.com/v3.1/all')
+.then(res=>res.json())
+.then(data=>console.log(data));
+
+},[])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default countries;
